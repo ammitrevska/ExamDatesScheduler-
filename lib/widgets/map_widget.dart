dd65757c2a1e1cbbccf22aaf4b1076a5c32a76d2
Ana-Marija M <ana-marija.mitrevska@students.finki.ukim.mk>
2024-02-19 20:58:27 +0100

    fastest route with google maps

--- lib/widgets/map_widget.dart
+++ lib/widgets/map_widget.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:latlong2/latlong.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class MapWidget extends StatefulWidget {
   const MapWidget({super.key});
@@ -10,6 +12,8 @@ class MapWidget extends StatefulWidget {
 }
 
 class _MapWidgetState extends State<MapWidget> {
+  final LatLng pinLocation = const LatLng(42.0046584, 21.4092858);
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -42,20 +46,43 @@ class _MapWidgetState extends State<MapWidget> {
               TileLayer(
                 urlTemplate: 'https://tile.openstreetmap.org/{z}/{x}/{y}.png',
               ),
-               MarkerLayer(
+              MarkerLayer(
                 markers: [
                   Marker(
-                    point: const LatLng(42.0046584, 21.4092858),
+                    point: pinLocation,
                     width: 80,
-                    height: 80, 
-                    child: Icon(Icons.pin_drop, color: Colors.indigo[800],),
+                    height: 80,
+                    child: Icon(Icons.pin_drop, color: Colors.indigo[800]),
                   )
                 ],
-              )
+              ),
             ],
+          ),
+          Positioned(
+            bottom: 16.0,
+            right: 16.0,
+            child: ElevatedButton(
+              onPressed: () {
+                launchGoogleMaps(pinLocation.latitude, pinLocation.longitude);
+              },
+              child: const Text('Navigate to Pin'),
+            ),
           ),
         ],
       ),
     );
+  }
+
+  Future<void> launchGoogleMaps(double latitude, double longitude) async {
+    final urlString =
+        'https://www.google.com/maps/dir/?api=1&destination=$latitude,$longitude'; // String URL
+    final uri = Uri.parse(urlString); // Convert string to Uri object
+
+    if (await canLaunchUrl(uri)) {
+      // Use Uri object with canLaunchUrl
+      await launchUrl(uri); // Use Uri object with launchUrl
+    } else {
+      throw 'Could not launch ${uri.toString()}'; // Use Uri object in error message
+    }
   }
 }
